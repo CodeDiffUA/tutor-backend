7e5268da9886e7d7553dbcda076324b620e02035
Bohdan <151460276+bodya4000@users.noreply.github.com>
2024-03-07 10:27:25 +0200

    initializing project

---
+++ src/test/java/dev/backend/tutor/TutorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.backend.tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
