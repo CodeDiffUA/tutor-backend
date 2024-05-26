90ea1a71efd80ffe85a48733b6ff30bcb6426bef
Bohdan <151460276+bodya4000@users.noreply.github.com>
2024-05-26 10:40:54 +0300

    get global ip endpoint

--- src/main/java/dev/backend/tutor/controllers/adviceControllers/QuickController.java
+++ src/main/java/dev/backend/tutor/controllers/adviceControllers/QuickController.java
@@ -18,9 +18,34 @@ public class QuickController {
 
 
     @GetMapping
-    public String getServiceIP() throws Exception {
+    public String getLocalServiceIP() throws Exception {
         InetAddress inetAddress = InetAddress.getLocalHost();
         String ipAddress = inetAddress.getHostAddress();
         return ipAddress;
+    }
+
+    @GetMapping("/global")
+    public String getGlobalServiceIP() throws Exception {
+        String urlString = "https://api.ipify.org";
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+        } else {
+            throw new IOException("Failed to get public IP address");
+        }
     }
 }
